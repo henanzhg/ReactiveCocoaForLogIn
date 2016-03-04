43ff2df9e4cbc5cab1c98f4fdb146f501757253e
henanzhg <854702773@qq.com>
2016-03-04 16:02:42 +0800

    Initial Commit

---
+++ ReactiveCocoa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReactiveCocoa
+//
+//  Created by mac on 3/4/16.
+//  Copyright © 2016 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
